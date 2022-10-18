eef3e915680940a8bc6b36135f101c8cf19c7954
Kaustub karki <kaustubkarki@gmail.com>
2022-10-19 00:08:31 +0545

    05 updated

--- 05/src/script.js
+++ 05/src/script.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
-import { AxesHelper } from "three";
+import { Group, Mesh, ObjectSpaceNormalMap } from "three";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -9,30 +9,58 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
+/**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.4;
-// mesh.position.z = -3;
-mesh.position.set(-0.7, -0.4, -0.2);
-//same thing
-scene.add(mesh);
+const group = new THREE.Group();
+group.scale.y = 1.5;
+group.rotation.y = 0.67;
+scene.add(group);
 
-const axesHelper = new THREE.AxesHelper();
-mesh.add(axesHelper);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1.5, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xf07070 })
+);
+cube1.position.x = -1.5;
+group.add(cube1);
 
-//xyz axis dekhauna
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1.5, 1),
+  new THREE.MeshBasicMaterial({ color: 0xce70f0 })
+);
+cube2.position.x = 0;
+group.add(cube2);
 
-console.log("mesh position ko normalise");
-//Convert this vector to a unit vector - that is, sets it equal to a vector with the same direction as this one, but length 1.
-// console.log(mesh.position.normalize());
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1.5),
+  new THREE.MeshBasicMaterial({ color: 0x70f0e2 })
+);
+cube3.position.x = 1.5;
+group.add(cube3);
 
-//scale
-mesh.scale.set(1, 1, 1);
-
+/*
+ * border
+ */
+//cube1
+var geo = new THREE.EdgesGeometry(cube1.geometry);
+var mat = new THREE.LineBasicMaterial({ color: 0x666666 });
+var wireframe = new THREE.LineSegments(geo, mat);
+cube1.add(wireframe);
+//cube2
+var geo = new THREE.EdgesGeometry(cube2.geometry);
+var mat = new THREE.LineBasicMaterial({ color: 0x666666 });
+var wireframe = new THREE.LineSegments(geo, mat);
+cube2.add(wireframe);
+//cube1
+var geo = new THREE.EdgesGeometry(cube3.geometry);
+var mat = new THREE.LineBasicMaterial({ color: 0x666666 });
+var wireframe = new THREE.LineSegments(geo, mat);
+cube3.add(wireframe);
 /**
  * Sizes
  */
@@ -42,28 +70,24 @@ const sizes = {
 };
 
 /**
+ * ROTAION
+ */
+group.rotation.set(0.5, -3.9, 0);
+group.scale.set(0.42, 0.62, 0.62);
+
+/**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-// camera.position.set(1, 1, 3);
-camera.position.z = 3;
+camera.position.z = 4;
+camera.lookAt(new THREE.Vector3(0, 2, -6));
 scene.add(camera);
-
-console.log("mest positon ko length");
-console.log(mesh.position.length());
-//length from center
-
-console.log("mesh position of the object and the camera");
-console.log(mesh.position.distanceTo(camera.position));
-//distance from the camera and the object created
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas,
+  canvas: canvas,
 });
-
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
-//26.13
